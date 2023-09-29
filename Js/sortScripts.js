64f1c962b9bd814d8032c4a4bb77dda5883c7595
Henrique G Albring <104649585+Skyramopdemo@users.noreply.github.com>
2023-09-29 16:29:43 +0000

    teste

--- Js/sortScripts.js
+++ Js/sortScripts.js
@@ -1,7 +1,78 @@
+/** AlreadSorted
+ * 
+ * @param {*} array 
+ * @returns 
+ */
 function alreadSorted(array){
   return array;
 }
+
+/** VoidSort
+ * 
+ * @param {*} array 
+ * @returns 
+ */
 function voidSort(array){
-  array = [0];
+  return array = [0];
+}
+
+/** IntegerSort
+ * 
+ * @param {*} array 
+ * @param {*} start 
+ * @returns 
+ */
+function integerSort(array, start) {
+  const n = [];
+  for (let i = 0; i < array.length; i++) {
+      n[i] = i + start;
+  }
+  return n;
+}
+
+/** CommunistSort
+ * 
+ * @param {*} array 
+ * @returns 
+ */
+function communistSort(array){
+  const sum = 0;
+  const arr = [];
+  for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+  }
+  const med = sum / array.length;
+  for (let i = 0; i < array.length; i++) {
+      arr[i] = med;
+  }
+  return arr;
+}
+
+/**
+ * 
+ * @param {*} array 
+ * @returns 
+ */
+function captalistSort(array) {
+  const sum = 0;
+  for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+      array[i] = 0;
+  }
+  array[0] = sum;
+  return array;
+}
+
+/**
+ * 
+ * @param {*} size 
+ * @param {*} minimun 
+ * @returns 
+ */
+function randNumberArray(size, minimun = 0){
+  const array = [];
+  for (let i = 0; i < size; i++) {
+      array[i] = Math.floor(Math.random() * (size - minimun) + minimun);
+  }
   return array;
 }
