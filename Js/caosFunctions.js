ba59d13c32a69e62a3983596d23c2a5e48e75ef8
Henrique G Albring <104649585+Skyramopdemo@users.noreply.github.com>
2023-10-24 18:26:46 +0000

    caosFunctions

---
+++ Js/caosFunctions.js
@@ -0,0 +1,18 @@
+
+
+function imTired() {
+    setTimeout(28800000);
+    return 'Waky Waky!';
+}
+
+function howinfinitylookslike(stop = false, limit = 0) {
+    let i = -1;
+    while (stop === false || i < limit) {
+        i++;
+        console.log(i);
+    }
+}
+
+function name(params) {
+    
+}
