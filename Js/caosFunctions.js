8b38db1ae8556b0f241bf12e25ae55735f41c339
Henrique G Albring <104649585+Skyramopdemo@users.noreply.github.com>
2023-10-31 17:03:11 +0000

    Add toBraile function

--- Js/caosFunctions.js
+++ Js/caosFunctions.js
@@ -1,5 +1,3 @@
-
-
 function imTired() {
     setTimeout(28800000);
     return 'Waky Waky!';
@@ -12,7 +10,13 @@ function howinfinitylookslike(stop = false, limit = 0) {
         console.log(i);
     }
 }
+// font: https://stackoverflow.com/questions/69756010/convert-string-to-braille
+let map = " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=".split("").reduce((o, n, i) => {
+return o[n] = "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"[i],
+    o[n.toLowerCase()] = o[n], o;
+}, {});
 
-function name(params) {
-    
+function toBraile(string) {
+    return string.split("").map(c => map[c]).join("");
 }
+
