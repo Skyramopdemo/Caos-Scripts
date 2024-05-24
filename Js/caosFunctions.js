106a121ba1f0917671cc75a7eccc4207c2357c86
Henrique G Albring <104649585+Skyramopdemo@users.noreply.github.com>
2024-05-24 16:58:40 +0000

    test page

--- Js/caosFunctions.js
+++ Js/caosFunctions.js
@@ -10,6 +10,7 @@ function howinfinitylookslike(stop = false, limit = 0) {
         console.log(i);
     }
 }
+
 // font: https://stackoverflow.com/questions/69756010/convert-string-to-braille
 let map = " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=".split("").reduce((o, n, i) => {
 return o[n] = "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"[i],
